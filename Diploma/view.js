8e0d80ea6c41274b65fea3472c75ff0f8bc58bdf
Thomas Siozos <34145662+SiozosThomas@users.noreply.github.com>
2020-04-11 18:50:22 +0300

    view.js
    View all versions

--- Diploma/view.js
+++ Diploma/view.js
@@ -3,25 +3,54 @@ google.charts.load('current', {packages:["orgchart"]});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Field');
-  data.addColumn('string', 'Father');
-  data.addRows([['root', '']]);
-  for (var key in data_0) {
-    if (data_0.hasOwnProperty(key)) {
-      data.addRows([[key + " : " + data_0[key], 'root']]);
-      if (data_0[key].constructor == objectConstructor) {
-        var nested = data_0[key];
-        for (var nested_key in nested) {
-          if (nested.hasOwnProperty(nested_key)) {
-            data.addRows([[nested_key + " : " + nested[nested_key], key + " : " + data_0[key]]]);
-          }
-        }
+  var versions = new Array(data_counter);
+  for (var i = 0; i <= data_counter; i++) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Field');
+    data.addColumn('string', 'Father');
+    data.addRows([['root', '']]);
+    getVersion(window["data_" + i], data, "root");
+    versions[i] = data;
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "version_" + i);
+    document.body.appendChild(newDiv);
+    //console.log(document.getElementById('version_' + i));
+  }
+  // var data = new google.visualization.DataTable();
+  // data.addColumn('string', 'Field');
+  // data.addColumn('string', 'Father');
+  // data.addRows([['root', '']]);
+  // for (var key in data_0) {
+  //   if (data_0.hasOwnProperty(key)) {
+  //     data.addRows([[key + " : " + data_0[key], 'root']]);
+  //     if (data_0[key].constructor == objectConstructor) {
+  //       var nested = data_0[key];
+  //       for (var nested_key in nested) {
+  //         if (nested.hasOwnProperty(nested_key)) {
+  //           data.addRows([[nested_key + " : " + nested[nested_key], key + " : " + data_0[key]]]);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  for (var i = 0; i <= data_counter; i++) {
+    var chart = new google.visualization.OrgChart(document.getElementById("version_" + i));
+    chart.draw(versions[i], {'allowHtml' : true})
+  }
+}
+
+function getVersion(version, data, father) {
+  var name = null;
+  for (var key in version) {
+    if (version.hasOwnProperty(key)) {
+      name = key + " : " + version[key];
+      data.addRows([[name, father]]);
+      if (version[key].constructor == objectConstructor) {
+        var nested = version[key];
+        getVersion(nested, data, name);
       }
     }
   }
-  var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
-  chart.draw(data, {'allowHtml' : true})
 }
 
 /*
