4172ee840dfa9eaa092e1d7682e291c9bf0748b2
SiozosThomas <siozosthomass@gmail.com>
2020-07-15 00:56:25 +0300

    clear documentation, update saving_project and presentation

---
+++ Export_and_Visualization_Schemas_Versions_from_JSON_Data/Saving Projects/city_inspections/output/versions.js
@@ -0,0 +1,109 @@
+var data_0 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "IntNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "IntNode",
+			"street" : "TextNode",
+			"number" : "IntNode",
+	},
+};
+var data_1 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "IntNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "TextNode",
+			"street" : "TextNode",
+			"number" : "TextNode",
+	},
+};
+var data_2 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "IntNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "TextNode",
+			"street" : "TextNode",
+			"number" : "IntNode",
+	},
+};
+var data_3 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "IntNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "IntNode",
+			"street" : "TextNode",
+			"number" : "TextNode",
+	},
+};
+var data_4 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "IntNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "IntNode",
+			"street" : "IntNode",
+			"number" : "TextNode",
+	},
+};
+var data_5 =
+{
+	"_id" : {
+			"$oid" : "TextNode",
+	},
+	"id" : "TextNode",
+	"certificate_number" : "TextNode",
+	"business_name" : "TextNode",
+	"date" : "TextNode",
+	"result" : "TextNode",
+	"sector" : "TextNode",
+	"address" : {
+			"city" : "TextNode",
+			"zip" : "IntNode",
+			"street" : "TextNode",
+			"number" : "IntNode",
+	},
+};
+var data_counter = 5;
--- Export_and_Visualization_Schemas_Versions_from_JSON_Data/output/versions.js
+++
@@ -1,8 +0,0 @@
-var data_0 =
-{
-	"caption" : "TextNode",
-	"photo_id" : "TextNode",
-	"business_id" : "TextNode",
-	"label" : "TextNode",
-};
-var data_counter = 0;
